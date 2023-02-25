8e81d2f1dd8723cf9acb90ac96f8230321dd3823
haris <haris.aulia404@gmail.com>
2023-02-25 21:57:29 +0700

    added @AllArgsConstructor

--- src/main/java/org/txtanon/entities/Sedan.java
+++ src/main/java/org/txtanon/entities/Sedan.java
@@ -1,5 +1,6 @@
 package org.txtanon.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @Builder
+@AllArgsConstructor
 public class Sedan extends Car {
     private int sedanSize;
 
