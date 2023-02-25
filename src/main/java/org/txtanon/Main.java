8200a998e718c6da4897987300d49e3ede79c271
haris <haris.aulia404@gmail.com>
2023-02-25 17:20:42 +0700

    re initiate

--- src/main/java/org/txtanon/Main.java
+++ src/main/java/org/txtanon/Main.java
@@ -1,7 +1,20 @@
 package org.txtanon;
 
+import org.txtanon.entities.Car;
+import org.txtanon.repository.ParkRepository;
+import org.txtanon.repository.ParkRepositoryImplement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car car = new Car();
+        car.setNumberPlate("BG 2017 AG");
+        List<List<Car>> listOfCar = new ArrayList<>();
+        ParkRepository<Car, String> parkRepository = new ParkRepositoryImplement(2);
+        Car save = parkRepository.save(car, 0);
+        System.out.println(save);
+
     }
 }
