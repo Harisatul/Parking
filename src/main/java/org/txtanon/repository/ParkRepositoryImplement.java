f5d9de18d250682c4d884affda994370445b18cd
haris <haris.aulia404@gmail.com>
2023-02-26 23:15:09 +0700

    changed data structure implementation using array; implemented findCarByPlateNumber method;implement getParkByFloor

--- src/main/java/org/txtanon/repository/ParkRepositoryImplement.java
+++ src/main/java/org/txtanon/repository/ParkRepositoryImplement.java
@@ -4,29 +4,48 @@ import org.txtanon.entities.Car;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ParkRepositoryImplement implements ParkRepository {
 
-    List<List<Car>> park;
+    List<Car[]> park;
 
     public ParkRepositoryImplement(int floor) {
         this.park = new ArrayList<>(floor);
         for (int i = 0; i < floor; i++) {
-            this.park.add(new ArrayList<>(5));
+            this.park.add(new Car[5]);
         }
     }
 
     @Override
-    public Car save(Car car, int floor) {
+    public Car save(Car car, int floor, int index) {
         if (floor == 1)
             floor = 0;
-        List<Car> cars = park.get(floor);
-        cars.add(car);
+        Car[] cars = park.get(floor);
+        cars[index] = car;
         return car;
     }
 
     @Override
     public void delete(String s) {
 
+    }
+
+    @Override
+    public Optional<Car> findCarByPlateNumber(String numberPLate) {
+        for (Car[] floor: park) {
+            for (int i = 0; i < floor.length; i++) {
+                if (floor[i].getNumberPlate().equals(numberPLate))
+                    return Optional.of(floor[i]);
+            }
+        }
+        return Optional.empty();
+    }
+
+    @Override
+    public Car[] getParkByFloor(int floor) {
+        if (floor == 1)
+            floor = 0;
+        return park.get(floor);
     }
 }
