1c37a9fbd35b81fc8828aab5c7f56c633822ca88
haris <haris.aulia404@gmail.com>
2023-02-26 23:24:39 +0700

    added saveSedanToRepositorySuccess test case

--- src/test/java/org/txtanon/repository/ParkRepositoryImplementTest.java
+++ src/test/java/org/txtanon/repository/ParkRepositoryImplementTest.java
@@ -1,11 +1,29 @@
 package org.txtanon.repository;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.txtanon.entities.Car;
+import org.txtanon.entities.Sedan;
 
 class ParkRepositoryImplementTest {
 
+    private ParkRepository<Car, String> parkRepository ;
+
+    @BeforeEach
+    void setUp() {
+        parkRepository = new ParkRepositoryImplement(2);
+    }
+
     @Test
-    void save() {
+    void saveSedanToParkSuccess() {
+        Sedan sedan = new Sedan();
+        sedan.setNumberPlate("BG 671 AS");
+        sedan.setColor("Blue");
+        sedan.setManufacture("Honda");
+        sedan.setYear("2017");
+        Car save = parkRepository.save(sedan, 0, 1);
+        Assertions.assertEquals(sedan.getNumberPlate(), save.getNumberPlate());
     }
 
     @Test
