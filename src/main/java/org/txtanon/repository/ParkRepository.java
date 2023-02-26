367f52552c597c83375e265361a34de340f2eb79
haris <haris.aulia404@gmail.com>
2023-02-26 23:13:29 +0700

    added index parameter in save method;defined findCarByPlateNumber method;defined getParkByFloor

--- src/main/java/org/txtanon/repository/ParkRepository.java
+++ src/main/java/org/txtanon/repository/ParkRepository.java
@@ -2,9 +2,13 @@ package org.txtanon.repository;
 
 import org.txtanon.entities.Car;
 
+import java.util.Optional;
+
 public interface ParkRepository<T extends Car, NumberPlate extends String> {
 
-    T save (T car, int floor);
+    T save (T car, int floor, int index);
     void delete (NumberPlate numberPlate);
+    Optional<T> findCarByPlateNumber(NumberPlate numberPlate);
+    Car[] getParkByFloor(int floor);
 
 }
