f359f7e1d08fde35a71df99c920440fb8466e5a3
haris <haris.aulia404@gmail.com>
2023-02-27 10:23:02 +0700

    Added TruckParkServiceImpl

---
+++ src/main/java/org/txtanon/service/TruckParkServiceImpl.java
@@ -0,0 +1,15 @@
+package org.txtanon.service;
+
+import org.txtanon.entities.Car;
+
+public class TruckParkServiceImpl implements ParkService {
+    @Override
+    public Car addCarToPark(Car car, int floor) throws Exception {
+        return null;
+    }
+
+    @Override
+    public Boolean deleteCarFromPark(String platNumber) throws Exception {
+        return null;
+    }
+}
