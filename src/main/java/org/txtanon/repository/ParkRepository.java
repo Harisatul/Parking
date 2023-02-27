150e5f2d5ce2e0c62959d50631034d5840a5e2d7
haris <haris.aulia404@gmail.com>
2023-02-27 09:41:55 +0700

    refactor code

--- src/main/java/org/txtanon/repository/ParkRepository.java
+++ src/main/java/org/txtanon/repository/ParkRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 public interface ParkRepository<T extends Car, NumberPlate extends String> {
 
     T save (T car, int floor, int index);
-    void delete (NumberPlate numberPlate);
+    Boolean delete (NumberPlate numberPlate);
     Optional<T> findCarByPlateNumber(NumberPlate numberPlate);
     Car[] getParkByFloor(int floor);
 
