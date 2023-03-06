da93f4d94d9cdb901a67b1e4f71bfd21de185c87
haris <haris.aulia404@gmail.com>
2023-03-06 13:54:35 +0700

    added ParkService documentation

--- src/main/java/org/txtanon/service/ParkService.java
+++ src/main/java/org/txtanon/service/ParkService.java
@@ -2,9 +2,33 @@ package org.txtanon.service;
 
 import org.txtanon.entities.Car;
 
+/**
+
+ The ParkService interface defines the methods for managing the parking lot.
+
+ It allows adding a car to a specific floor of the parking lot and deleting a car from the parking lot.
+
+ @author haris
+  */
 public interface ParkService {
 
+
+    /**
+         Adds a car to the parking lot on the specified floor.
+
+         @param car the car object to be added to the parking lot
+         @param floor the floor where the car should be parked
+         @return the car object that has been parked in the parking lot
+         @throws Exception if the parking lot is full.
+     */
     Car addCarToPark(Car car, int floor) throws Exception;
+    /**
+     Delete a car to the parking lot on the specified floor.
+
+     @param platNumber the car object to be added to the parking lot
+     @return the car object that has been parked in the parking lot
+     @throws Exception if the parking lot is full.
+     */
     Boolean deleteCarFromPark(String platNumber) throws Exception;
 
 }
