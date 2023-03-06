32336ef80278df71afd3cc9b68ed344af19bbc8f
haris <haris.aulia404@gmail.com>
2023-03-06 13:03:00 +0700

    added documentation for Truck class

--- src/main/java/org/txtanon/entities/Truck.java
+++ src/main/java/org/txtanon/entities/Truck.java
@@ -2,13 +2,30 @@ package org.txtanon.entities;
 
 import lombok.*;
 
+
+/**
+ * Represents a Truck which is a subclass of Car with an additional attribute for the truck size.
+ * @see Car
+ */
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class Truck extends Car{
 
+    /**
+     * The size of the Truck.
+     * Default value is 2.
+     */
     private int truckSize = 2;
 
+    /**
+     * Initializes a new instance of the Truck class with the specified values for the fields.
+     *
+     * @param numberPlate The number plate of the Truck.
+     * @param color The color of the Truck.
+     * @param manufacture The manufacture of the Truck.
+     * @param year The year of production of the Truck.
+     */
     public Truck(String numberPlate,String color,
                  String manufacture, String year) {
         super(numberPlate, color, manufacture, year);
