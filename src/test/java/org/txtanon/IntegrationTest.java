64923224705442447ce954fd422e49962d63eb9b
haris <haris.aulia404@gmail.com>
2023-03-06 09:11:01 +0700

    added integrationWhenRemoveTruckSuccess test case

--- src/test/java/org/txtanon/IntegrationTest.java
+++ src/test/java/org/txtanon/IntegrationTest.java
@@ -189,9 +189,47 @@ public class IntegrationTest {
         // Now on the floor 2.
         // [Sedan, Truck, Truck, null, Sedan] == floor 2
         Car[] parkByFloor = parkRepository.getParkByFloor(2);
-        // we check in floor to index-3 is null
+        // we check in floor two, index-3 is null
         Assertions.assertNull(parkByFloor[3]);
     }
 
+    @Test
+    void integrationWhenRemoveTruckSuccess() throws Exception {
+        Sedan sedan = new Sedan();
+        sedan.setNumberPlate("BG 223 SS");
+
+        Truck truck = new Truck();
+        truck.setNumberPlate("BG 543 S");
+
+        // Added To Floor 2
+        Car savedSedan = sedanParkingService.addCarToPark(sedan, 2);
+        Car savedTruck = truckParkService.addCarToPark(truck, 2);
+
+        Sedan sedan2 = new Sedan();
+        sedan2.setNumberPlate("B 532 SD");
+
+        Sedan sedan3 = new Sedan();
+        sedan3.setNumberPlate("B 7411 RT");
+
+        // Added To Floor 2
+        Car savedSedan2 = sedanParkingService.addCarToPark(sedan2, 2);
+        Car savedSedan3 = sedanParkingService.addCarToPark(sedan3, 2);
+
+        /*
+         * so, now on floor two. The parking lot has occupied
+         * [null, null, null, null, null] == floor 1
+         * [Sedan, Truck, Truck, Sedan, Sedan] == floor 2
+         * */
+
+        // Now, we wanna remove truck with plate number "B 532 SD" from parking lot.
+        truckParkService.deleteCarFromPark("BG 543 S");
+
+        // Now on the floor 2.
+        // [Sedan, Truck, Truck, null, Sedan] == floor 2
+        Car[] parkByFloor = parkRepository.getParkByFloor(2);
+        // we check on floor two, index-2 and index-3 is null
+        Assertions.assertNull(parkByFloor[1]);
+        Assertions.assertNull(parkByFloor[2]);
+    }
 
 }
