5e7d61e51aedd59a996743b16c7234505c1c7ebd
haris <haris.aulia404@gmail.com>
2023-03-06 12:57:41 +0700

    added documentation for Car class

--- src/main/java/org/txtanon/entities/Car.java
+++ src/main/java/org/txtanon/entities/Car.java
@@ -5,15 +5,35 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+/**
+ * This base class. Represents a car with its number plate, color, manufacture, and year of production.
+ * @author haris
+ * @see Sedan
+ * @see Truck
+ * */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Car {
 
+    /**
+     * The number plate of the car.
+     */
     private String numberPlate;
+    /**
+     * The color of the car.
+     */
     private String color;
+    /**
+     * The manufacture of the car.
+     */
     private String manufacture;
+    /**
+     * The year of production of the car.
+     */
     private String year;
+
 
 }
