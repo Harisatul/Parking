4a1e66d6ceb78ac05827bc711b342b217bbdbd14
haris <haris.aulia404@gmail.com>
2023-03-06 08:47:04 +0700

    added integrationWhenRemoveSedanSuccess test case

--- src/test/java/org/txtanon/IntegrationTest.java
+++ src/test/java/org/txtanon/IntegrationTest.java
@@ -40,7 +40,7 @@ public class IntegrationTest {
         sedan2.setNumberPlate("B 532 SD");
 
         Sedan sedan3 = new Sedan();
-        sedan2.setNumberPlate("B 7411 RT");
+        sedan3.setNumberPlate("B 7411 RT");
 
         // Added To Floor 2
         Car savedSedan2 = sedanParkingService.addCarToPark(sedan2, 2);
@@ -148,14 +148,50 @@ public class IntegrationTest {
 
         // but, we can still add sedan on floor two
         Sedan sedan4 = new Sedan();
-        sedan3.setNumberPlate("B 823 ST");
+        sedan4.setNumberPlate("B 823 ST");
         Car car = sedanParkingService.addCarToPark(sedan4, 2);
 
         Assertions.assertEquals(sedan4.getNumberPlate(), car.getNumberPlate());
 
     }
 
+    @Test
+    void integrationWhenRemoveSedanSuccess() throws Exception {
+        Sedan sedan = new Sedan();
+        sedan.setNumberPlate("BG 223 SS");
 
+        Truck truck = new Truck();
+        truck.setNumberPlate("BG 543 S");
+
+        // Added To Floor 2
+        Car savedSedan = sedanParkingService.addCarToPark(sedan, 2);
+        Car savedTruck = truckParkService.addCarToPark(truck, 2);
+
+        Sedan sedan2 = new Sedan();
+        sedan2.setNumberPlate("B 532 SD");
+
+        Sedan sedan3 = new Sedan();
+        sedan3.setNumberPlate("B 7411 RT");
+
+        // Added To Floor 2
+        Car savedSedan2 = sedanParkingService.addCarToPark(sedan2, 2);
+        Car savedSedan3 = sedanParkingService.addCarToPark(sedan3, 2);
+
+        /*
+         * so, now on floor two. The parking lot has occupied
+         * [null, null, null, null, null] == floor 1
+         * [Sedan, Truck, Truck, Sedan, Sedan] == floor 2
+         * */
+
+        // Now, we wanna remove sedan with plate number "B 532 SD" from parking lot.
+        sedanParkingService.deleteCarFromPark("B 532 SD");
+
+        // Now on the floor 2.
+        // [Sedan, Truck, Truck, null, Sedan] == floor 2
+        Car[] parkByFloor = parkRepository.getParkByFloor(2);
+        // we check in floor to index-3 is null
+        Assertions.assertNull(parkByFloor[3]);
+    }
 
 
 }
