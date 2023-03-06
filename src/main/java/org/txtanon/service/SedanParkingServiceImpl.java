299910070749af95813eb619d5d13ad021cba720
haris <haris.aulia404@gmail.com>
2023-03-06 14:24:10 +0700

    added SedanParkingServiceImplarkService documentation

--- src/main/java/org/txtanon/service/SedanParkingServiceImpl.java
+++ src/main/java/org/txtanon/service/SedanParkingServiceImpl.java
@@ -6,15 +6,35 @@ import org.txtanon.entities.Sedan;
 import org.txtanon.repository.ParkRepository;
 
 
+/**
+ * Implementation of ParkService interface for Sedan cars.
+ * @author haris
+ */
 @Slf4j
 public class SedanParkingServiceImpl implements ParkService {
 
     private final ParkRepository<Sedan, String> parkRepository;
 
+    /**
+     * Constructor to set parkRepository
+     * @param parkRepository Repository to manage park
+     */
     public SedanParkingServiceImpl(ParkRepository<Sedan, String> parkRepository) {
         this.parkRepository = parkRepository;
     }
 
+
+    /**
+     * Add Sedan car to the park
+     * The idea is, iterate through floor and specified floor to find empty space. if empty space was found
+     * we assign that space with new Object of Sedan. There is check if the parking lot has already
+     * same Car that we want to add. Then, check if park is full. return exception
+     *
+     * @param car Sedan car to add
+     * @param floor floor in which to park the car
+     * @return added Sedan car
+     * @throws Exception if Sedan car is already in park or park is full
+     */
     @Override
     public Car addCarToPark(Car car, int floor) throws Exception {
         boolean carByPlateNumber = parkRepository.findCarByPlateNumber(car.getNumberPlate()).isPresent();
@@ -35,6 +55,15 @@ public class SedanParkingServiceImpl implements ParkService {
         throw new Exception("park is full");
     }
 
+    /**
+     * Delete Sedan car from the park
+     * the idea is to call and pass the value to parkRepository.delete(platNumber)
+     *
+     * @param platNumber number plate of Sedan car to delete
+     * @return true if Sedan car is successfully deleted, false if Sedan car is not found in the park
+     * @throws Exception if Sedan car is not found in the park
+     *
+     */
     @Override
     public Boolean deleteCarFromPark(String platNumber) throws Exception {
         if (parkRepository.delete(platNumber)){
